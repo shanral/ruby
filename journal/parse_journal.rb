eb98f204e892825a999471cdfb6a63412695cde0
Andrew Loucky <andrew.loucky@gmail.com>
2014-01-21 20:26:20 -0800

    parse_journal.rb, store accurately, but not calculate sleep

--- journal/parse_journal.rb
+++ journal/parse_journal.rb
@@ -1,16 +1,19 @@
+#!/usr/bin/env ruby
+
 file = open("journal.txt")
 
 sleep_start = ""
 sleep_end   = ""
 file.each_line do |line|
-  if line =~ /\d{6}/
+  if line =~ /\A\d{6} \(\w{3}\):\n\z/
     sleep_start = ""
     sleep_end   = ""
   elsif line =~ /\AAwake at (\d{4})\n\z/
     sleep_start = $1
   elsif line =~ /\AAsleep at (\d{4})\n\z/
     sleep_end = $1
-    p "Awake at #{sleep_start}, asleep at #{sleep_end}"
+    puts "Awake at #{sleep_start}, asleep at #{sleep_end}"
+    puts "#{sleep_end.to_i - sleep_start.to_i}"
   end
 end
 
