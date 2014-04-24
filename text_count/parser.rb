31243a95d55730c23c0e8ecdf922099ae8238a97
Andrew Loucky <andrew.loucky@gmail.com>
2014-04-24 14:28:53 -0700

    text_count, parser updates, but not functional

--- text_count/parser.rb
+++ text_count/parser.rb
@@ -1,12 +1,32 @@
-#!/usr/bin/env ruby
+require 'minitest/autorun'
 
-f = File.read(ARGV[0])
+class WordCount
 
-output = f.scan(/\w+/).group_by{ |word| word.downcase }.map { |k, v| [k, v.size]}
-output.sort_by!{ |k| k[1] }.reverse!
+  def initialize(file)
+    @file = file
+  end
 
-file = File.new('output.txt', 'w')
-output.each do |word|
-  file.write("#{word[1]}, #{x[0]}\n")
+  def raw_word_count
+    @file.scan(/\w+/).group_by { |word| word.downcase }.map { |k, v| [k, v.size] }
+  end
+
+  def sorted_word_count
+    raw_word_count.sort_by{ |k| k[1] }.reverse
+  end
 end
 
+
+describe WordCount do
+  let(:ex1) { WordCount.new('aaa aa a a a') }
+
+  it "should create an accurate unsorted array" do
+    ex1.raw_word_count.must_equal [['aaa', 1], ['aa', 1], ['a', 3]]
+  end
+
+  it "should create an accurate sorted array" do
+    ex1.sorted_word_count.must_equal [['a', 3], ['aa', 1], ['aaa', 1]]
+  end
+
+  let(:ex2) { WordCount.new("can't won't wont cant") }
+  # how to handle these...
+end
