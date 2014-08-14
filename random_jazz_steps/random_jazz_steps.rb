f5854a3a5ed56161066467b10ad10edaa6023d20
Andrew Loucky <andrew.loucky@gmail.com>
2014-08-14 19:39:45 -0400

    remove explicit path for moves.txt, some reorganization

--- random_jazz_steps/random_jazz_steps.rb
+++ random_jazz_steps/random_jazz_steps.rb
@@ -1,5 +1,3 @@
 NUMBER_OF_MOVES = ARGV[0].to_i
-URL = "/home/ubuntu/programming/ruby/random_jazz_moves/moves.txt"
-
-moves = `shuf #{URL} -n #{NUMBER_OF_MOVES}`
-moves.split("\n").each { |move| puts ">> #{move}" }
+moves = `shuf moves.txt -n #{NUMBER_OF_MOVES}`.split("\n")
+moves.each { |move| puts ">> #{move}" }
